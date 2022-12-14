c24e08715a11194a5d61cbc5d0057eb3818eff06
Abul Basar <adulbashar124@gmail.com>
2022-12-14 21:12:56 +0600

    quantity update

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { RiDeleteBin2Fill } from "react-icons/ri";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import {
+  addToCart,
+  removeFromCart,
+} from "../redux/actionCreators/productAction";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
   return (
     <div
       className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
@@ -15,14 +24,36 @@ const ProductCard = ({ product }) => {
       <div className=" flex-1">
         <ul className="space-y-2">
           {product.keyFeature.map((feature) => {
-            return <li className="text-sm ">{feature}</li>;
+            return (
+              <li key={feature} className="text-sm ">
+                {feature}
+              </li>
+            );
           })}
         </ul>
       </div>
       <div className="flex gap-2 mt-5">
-        <button className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold">
-          Add to cart
-        </button>
+        {pathname === "/" && (
+          <button
+            onClick={() => {
+              dispatch(addToCart(product));
+            }}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+        {pathname === "/cart" && (
+          <button
+            onClick={() => {
+              dispatch(removeFromCart(product));
+            }}
+            className="bg-red-400  rounded-full py-1 px-5 flex-1 text-white text-bold flex justify-between items-center "
+          >
+            <p>Remove</p>
+            <RiDeleteBin2Fill size={20} />
+          </button>
+        )}
         <button
           title="Add to wishlist"
           className="bg-indigo-500  py-1 px-2 rounded-full"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
-    <div>
-      <h1>home</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {products.map((product) => (
+        <ProductCard product={product} key={product._id}></ProductCard>
+      ))}
     </div>
   );
 };
